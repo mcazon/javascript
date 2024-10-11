66ae9be3a75c2a34c8892f75acb0ae76a57c98ee
Mário Cézar <163690025+mcazon@users.noreply.github.com>
2024-10-11 06:25:19 -0300

    IF

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+
+if (vel > 60) { //condição simples
+    console.log (`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "BRASIL"
+
+if (pais == "BRASIL") {
+    console.log (`Você é Brasileiro`)
+} else {
+    console.log ('Você é Estrangeiro')
+}
