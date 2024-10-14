9084bde96b4f2167c29c8acb5bd4bbf55fd0ffa3
Mário Cézar <163690025+mcazon@users.noreply.github.com>
2024-10-14 07:29:12 -0300

    Condições Multiplas

---
+++ aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 24
+    console.log (`Você tem ${idade} anos`)
+if (idade < 16) {
+    console.log ('Não Vota')
+} else if (idade <= 17 || idade >= 65){
+     console.log ('Voto opcional') 
+} else {
+    console.log('Voto Obrigatório!')
+}
+
+
