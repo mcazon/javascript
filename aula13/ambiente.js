67db41b23f23b27c995a24070d04640d237c0079
Mário Cézar <163690025+mcazon@users.noreply.github.com>
2024-10-18 06:51:39 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 8)
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
